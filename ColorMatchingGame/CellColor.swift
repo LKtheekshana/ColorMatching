62110b4cc5c2849075135c2ef3359683567bcffb
Theekshana <Isuruace@gmail.com>
2026-01-16 16:57:00 +0530

    Add color-based grid gameplay and win condition

---
+++ ColorMatchingGame/CellColor.swift
@@ -0,0 +1,8 @@
+//
+//  CellColor.swift
+//  ColorMatchingGame
+//
+//  Created by COBSCCOMP242P-031 on 2026-01-16.
+//
+
+import Foundation
---
+++ ColorMatchingGame/GameView.swift
@@ -0,0 +1,8 @@
+//
+//  GameView.swift
+//  ColorMatchingGame
+//
+//  Created by COBSCCOMP242P-031 on 2026-01-16.
+//
+
+import Foundation
